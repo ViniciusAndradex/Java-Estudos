c4864170c62dccac1426008b6bf1bca549f8dab4
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-06-30 15:40:18 -0300

    Seção 1, concluída.

--- estruturas_java/src/Main.java
+++ estruturas_java/src/Main.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello, World!");
 
+        System.out.print("Digite o nome do autor: ");
+        String x = sc.next();
+        System.out.printf("Seu nome é %s%n", x);
+
+        sc.close();
     }
 }
