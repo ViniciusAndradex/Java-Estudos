3e68a2f96a7589bbf052f30c0548f9b8f8a784cf
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-07-04 15:58:26 -0300

    Construtores e encapsulamento adicionados.

--- POO/src/estoque/Product.java
+++ POO/src/estoque/Product.java
@@ -1,10 +1,37 @@
 package estoque;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
     public static final float PI = 3.14f;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
 
     public double totalValueInStock(){
         return quantity * price;
