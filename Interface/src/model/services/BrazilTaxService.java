a5c2eb611e4b641357824e4ebbad1c97ee731d21
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-07-20 17:30:34 -0300

    Implementing interfaces on Tax for more clean code and abstraction.

--- Interface/src/model/services/BrazilTaxService.java
+++ Interface/src/model/services/BrazilTaxService.java
@@ -1,6 +1,8 @@
 package model.services;
 
-public class BrazilTaxService {
+import model.interfaces.TaxService;
+
+public class BrazilTaxService implements TaxService {
 
     public double tax(double amount) {
         return (amount > 100) ? amount * 0.15: amount * 0.2;
--- Interface/src/model/services/RentalService.java
+++ Interface/src/model/services/RentalService.java
@@ -2,6 +2,7 @@ package model.services;
 
 import model.entities.CarRental;
 import model.entities.Invoice;
+import model.interfaces.TaxService;
 
 import java.time.Duration;
 
@@ -9,9 +10,9 @@ public class RentalService {
 
     private Double pricePerHour;
     private Double pricePerDay;
-    private BrazilTaxService taxService;
+    private TaxService taxService;
 
-    public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+    public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
         this.pricePerHour = pricePerHour;
         this.pricePerDay = pricePerDay;
         this.taxService = taxService;
